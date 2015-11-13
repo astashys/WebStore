9721957f666527ee7791e85483de19890ce911d2
astashys <astashys@gmail.com>
2015-11-13 22:52:23 +0200

    - Added embedded database datasource - Removing redundant dependencies

---
+++ src/main/webapp/WEB-INF/db/sql/insert-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users VALUES (1, 'astash', 'astash@gmail.com');
+INSERT INTO users VALUES (2, 'pborovet', 'pborovet@yahoo.com');
+INSERT INTO users VALUES (3, 'rtserk', 'rtserk@gmail.com');
