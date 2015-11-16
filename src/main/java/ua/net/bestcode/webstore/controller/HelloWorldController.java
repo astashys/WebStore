bfd7a1621ccca8898d20fc53f147dc5a6381377a
astashys <astashys@gmail.com>
2015-11-16 21:42:03 +0200

    Added sl4j logging based on logback. It will zip logs every day.

--- src/main/java/ua/net/bestcode/webstore/controller/HelloWorldController.java
+++ src/main/java/ua/net/bestcode/webstore/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package ua.net.bestcode.webstore.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,12 +10,16 @@ import ua.net.bestcode.webstore.manager.IUserManager;
 
 @Controller
 public class HelloWorldController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorldController.class);
+
     @Autowired
     IUserManager userManager;
 
 
     @RequestMapping("/")
     public String hello(Model model) {
+        LOG.debug("++++++++++");
         int count = userManager.addUserAndReturnCount();
         model.addAttribute("count", count);
 
